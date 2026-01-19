196e6e6eb714571cfa87e823963b64ccb9c976e7
Amarea <Amarea1103@gmail.com>
2026-01-18 20:51:09 -0500

    add

--- script.js
+++ script.js
@@ -7,8 +7,18 @@ keys.forEach((key) => {
 });
 
 function playNote(key) {
-  const noteAudio = document.getElementById(key.dataset.note);
+//  get the notes letter value
+    const noteAudio = document.getElementById(key.dataset.note);
+    // reset the notes current time to 0
+  noteAudio.currentTime = 0;
+
+//  play the note
   noteAudio.play();
 
-  
+//add a class, active for styling purposes
+ key.classList.add('active');
+// listen for when the sound has ended and remove active class
+  noteAudio.addEventListener('ended', () => {
+    key.classList.remove('active');
+  });
 }
