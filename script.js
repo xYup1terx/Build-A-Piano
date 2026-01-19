4aeba388750d6f84d0294b93fd84daf65edfb4ca
Amarea <Amarea1103@gmail.com>
2026-01-18 21:02:55 -0500

    Add keybinds

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//vars
+const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
+
+const whiteKeys = document.querySelectorAll('.key.white');
+const blackKeys = document.querySelectorAll('.key.black');
+
 // select all keys
 const keys = document.querySelectorAll('.key');
 
@@ -22,3 +29,18 @@ function playNote(key) {
     key.classList.remove('active');
   });
 }
+
+document.addEventListener('keydown', (e) => {
+  // Get the key that triggered the event
+  const key = e.key;
+  const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+  const blackKeyIndex = BLACK_KEYS.indexOf(key);
+
+  if (whiteKeyIndex > -1) {
+    playNote(whiteKeys[whiteKeyIndex]);
+  }
+
+  if (blackKeyIndex > -1) {
+    playNote(blackKeys[blackKeyIndex]);
+  }
+});
